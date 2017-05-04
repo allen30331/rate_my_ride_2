a6b2bceeb601366ace412871b969f262ce6ec9a1
allen30331 <allen30331@gmail.com>
2017-05-04 16:48:20 -0400

    added render function to results page

--- public/js/results.js
+++ public/js/results.js
@@ -8,7 +8,7 @@ $('form').submit(function() {
 
 
 
-/////Gets driver bby the drivers tag number/////
+/////Gets driver by the drivers tag number/////
 function getDriver(driverTagNumber, callbackFn) {
   $.ajax({
     url: `/drivers/${driverTagNumber}/tagNumber`,  //http://localhost:8080
@@ -34,7 +34,61 @@ function getDriver(driverTagNumber, callbackFn) {
 
 
 function renderData(data) {
-	console.log(data);
+	
+	let now = new Date();
+  
+	let day = now.getDate();
+
+  	let month = now.getMonth();
+
+  	let year = now.getFullYear();
+
+
+
+	if (data.status === 500) {
+		$('.modal').show();
+	}
+	else {
+		$('.driver-results').append(
+			`<div class="row">
+				<div class="col-12">
+					<h1>${data.driverName}</h1>
+					<button class="review-driver-button">Review Driver</button>
+					<h3>${data.company}</h3>
+					<h3>tag number: ${data.tagNumber}</h3>
+					<h3>city: ${data.city}</h3>
+					<h3>rating: ${data.averageDriverRating}</h3>
+					<h3 class="driver-description">Tags for this driver</h3></div>
+				</div>
+			</div>`
+		);
+
+
+		for (key in data.descriptionSummary) {
+    		$('.driver-results').append(
+          `<div class="row">
+	          	<div class="col-12">
+	          		<h3>${key}: ${data.descriptionSummary[key]}</h3>
+	          	</div>
+          </div>`);
+  		}
+
+
+	    data.reviews.forEach(function (review) {
+		    $('.driver-results').append(
+	          `<div class="row">
+		          	<div class="col-12">
+		          		<p>rating: ${review.driverRating}</p>
+		          		<p>description: ${review.description}</p>
+		          		<p>comment: ${review.comment}</p>
+		          		<p>created: ${month}/${day}/${year}</p>
+		          		<div class="border"></div>
+		          	</div>
+	          </div>`
+	        )
+	        
+	    });
+	}
 }
 
 
