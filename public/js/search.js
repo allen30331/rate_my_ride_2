2289422893732edda134d875401a57403026f188
allen30331 <allen30331@gmail.com>
2017-05-15 20:52:45 -0400

    removed console.log from search function

--- public/js/search.js
+++ public/js/search.js
@@ -1,13 +1,7 @@
 function searchForDriver() {
-	// $('form').submit(function() {
-		let tagNumber = $('form').find('.search-field').val();
-		//alert(tagNumber);
-		console.log(tagNumber, 'hello');
-		let myStorage = localStorage.setItem("query", tagNumber);
-	// });
+	let tagNumber = $('form').find('.search-field').val();
+	console.log(tagNumber, 'hello');
+	let myStorage = localStorage.setItem("query", tagNumber);
 }
 
-// function searchForDriver() {
-// 	alert('hi');
-// }
 
